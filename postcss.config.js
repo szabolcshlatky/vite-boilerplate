7931313ec4286b39374e64c68b2de7e1be07e2fe
szabolcshlatky <szabolcshlatky@gmail.com>
2024-01-15 12:33:47 +0100

    (re)init

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    'postcss-import': {},
+    // 'postcss-preset-env': {},
+    // 'postcss-scss': {},
+    cssnano: {
+      preset: 'default',
+    },
+  },
+};
---
+++ src/counter/counter.ts
@@ -0,0 +1,37 @@
+import styles from './counter.module.css';
+import javascriptLogo from './javascript.svg';
+import typescriptLogo from './typescript.svg';
+import viteLogo from "/vite.svg";
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Vite + TypeScript + JavaScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`;
+
+function setupCounter(element: HTMLButtonElement) {
+  let counter = 0
+  const setCounter = (count: number) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  element.addEventListener('click', () => setCounter(counter + 1))
+  setCounter(0)
+}
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
